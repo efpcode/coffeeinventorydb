6ccfcbcce13ce89f9c9f43929a1757c22c96d1f4
efpcode <efpjobhunt@gmail.com>
2024-12-05 14:43:55 +0100

    Added dbconnect method

--- src/src/CoffeeInventoryDB.java
+++ src/src/CoffeeInventoryDB.java
@@ -1,5 +1,24 @@
+
+import java.sql.*;
+import java.util.Scanner;
+
 public class CoffeeInventoryDB {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Connection dbconnect(){
+        String url = "jdbc:sqlite:/Users/efpdev/Documents/iths/sqldatabasecourse/labbar/lab3/coffeebeans.db";
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url);
+        }catch (SQLException e){
+            System.out.println("Something went wrong");
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Connection dbc = dbconnect();
     }
 }
